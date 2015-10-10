8c067e74dd7c5d08aca7b0a7fe9cd1b6f8487ba1
Harkanwal Narula <narula.harkanwal@gmail.com>
2015-10-10 16:53:05 -0400

    still working on azure

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
-/// <reference path="game.js" />
-var express = require("express"),
-    app = express();
+
+var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io').listen(server);
 
-server.listen(3000);
-app.use("/Scripts", express.static(__dirname + '/Scripts'));
-app.use("/", express.static(__dirname + '/'));
+server.listen(4004);
+
 app.get('/', function (req, res) {
     res.sendfile(__dirname + '/index.html');
 });
