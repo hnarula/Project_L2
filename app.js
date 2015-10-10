f24f69cc59ec2470d914ddc7ef50de7728916d0c
Harkanwal Narula <narula.harkanwal@gmail.com>
2015-10-10 15:54:58 -0400

    Node Js Server working on

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 /// <reference path="game.js" />
-var app = require('express')();
+var express = require("express"),
+    app = express();
 var server = require('http').Server(app);
 var io = require('socket.io').listen(server);
 
 server.listen(3000);
-
+app.use("/Scripts", express.static(__dirname + '/Scripts'));
+app.use("/", express.static(__dirname + '/'));
 app.get('/', function (req, res) {
     res.sendfile(__dirname + '/index.html');
 });
