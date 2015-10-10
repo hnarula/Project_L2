0bc256385aab385d46203a3022c51c3bd783d520
Harkanwal Narula <narula.harkanwal@gmail.com>
2015-10-10 12:57:43 -0400

    Port Change

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io').listen(server);
 
-server.listen(4004);
+server.listen(3000);
 
 app.get('/', function (req, res) {
     res.sendfile(__dirname + '/index.html');
