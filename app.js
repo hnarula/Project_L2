df274698eec8c13e3886ad14630caf49b66d371c
Harkanwal Narula <narula.harkanwal@gmail.com>
2015-10-11 12:17:35 -0400

    still testing

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 var express = require('express');
+var http = require('http');
 var app = express();
+
+app.set('port', process.env.PORT || 3000);
+var httpServer = http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
-});
