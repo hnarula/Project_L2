c178c945648f772c100686a8880017b1ce9b1cc4
Harkanwal Narula <narula.harkanwal@gmail.com>
2015-10-11 12:23:04 -0400

    node server should run on azure with this

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ var express = require('express');
 var http = require('http');
 var app = express();
 
+
 app.set('port', process.env.PORT || 3000);
 var httpServer = http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
 
+
+app.use("/Scripts", express.static(__dirname + '/Scripts'));
+app.use("/", express.static(__dirname + '/'));
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+    res.sendfile(__dirname + '/index.html');
 });
 
+var io = require('socket.io').listen(httpServer);
