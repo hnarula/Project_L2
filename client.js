f89b4843438971943685895cf5f51479f861d831
Harkanwal Narula <narula.harkanwal@gmail.com>
2015-10-11 22:01:59 -0400

    client-server send/receive message working

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
 
-var socket = io.connect('http://pl2cd.azurewebsites.net/');
-//var socket = io.connect('http://localhost:3000/');
+var socket = io.connect('https://pl2cd.azurewebsites.net:443/'); // for azure
+//var socket = io.connect('http://localhost:3000/'); // for local server
 function connect() {
     var name = $("#name").val();
     socket.emit('newplayer', { name: name });
